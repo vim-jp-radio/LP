fd53d5184eb2b47b68f8aebd6133b264537c9e1d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-27 13:50:36 +0100

    feat(layout): add Header component to layout
    A new Header component has been added to the layout. This component
    includes meta tags for Twitter and Open Graph, and it also includes
    the title and description of the site. The Header component is now
    imported and used in the layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,11 @@
 	import 'uno.css';
 	import '@unocss/reset/normalize.css';
 
+	import Header from './Header.svelte';
+
 	const { children } = $props();
 </script>
+
+<Header />
 
 {@render children()}
---
+++ src/routes/Header.svelte
@@ -0,0 +1,52 @@
+<script lang='ts'>
+	import Logo from '$/assets/vimjp-radio-cover-art/3000x3000-fs8.png';
+
+	const title = `エンジニアの楽園 vim-jpラジオ`;
+	const description = `2024年7月8日配信開始のラジオ番組`;
+
+	const xAccount = `@vimjpradio`;
+	const locale = `ja_JP`;
+
+	const images = [{ src: Logo, alt: `vim-jpラジオのロゴ`, type: `image/png` }] as const;
+</script>
+
+<!-- X のメタタグを生成するsnippet -->
+{#snippet xMeta(property: string, content: string)}
+	<meta name='twitter:{property}' content={content} />
+{/snippet}
+
+<!-- Open Graph のメタタグを生成するsnippet -->
+{#snippet ogMeta(property: string, content: string)}
+	<!-- eslint-disable svelte/sort-attributes -->
+	<meta property='og:{property}' content={content} />
+	<!-- eslint-enable svelte/sort-attributes -->
+{/snippet}
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name='description' content={description} />
+
+	<!-- X -->
+	{@render xMeta('card', 'summary_large_image')}
+	{@render xMeta('site', xAccount)}
+	{@render xMeta('creator', xAccount)}
+	{@render xMeta('title', title)}
+	{@render xMeta('description', description)}
+	{#each images as { src, alt, type } (src)}
+		{@render xMeta('image', src)}
+		{@render xMeta('image:alt', alt)}
+		{@render xMeta('image:type', type)}
+	{/each}
+
+	<!-- Open Graph -->
+	{@render ogMeta('type', 'website')}
+	{@render ogMeta('title', title)}
+	{@render ogMeta('description', description)}
+	{@render ogMeta('locale', locale)}
+	{@render ogMeta('image', Logo)}
+	{#each images as { src, alt, type } (src)}
+		{@render ogMeta('image', src)}
+		{@render ogMeta('image:alt', alt)}
+		{@render ogMeta('image:type', type)}
+	{/each}
+</svelte:head>
