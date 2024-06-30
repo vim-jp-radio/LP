526b815eef33c0c2ac49f915aca7a00f51146b22
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-30 16:02:05 +0100

    feat(assets): add alisue ASCII art and log it in layout
    This commit introduces a new ASCII art named 'alisue' in the assets
    directory. The art is then imported and logged in the console within
    the layout component. This is mainly for visual debugging purposes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,14 @@
 	import '@unocss/reset/normalize.css';
 
 	import Header from './Header.svelte';
+	import { ALISUE } from '$/assets/aa';
 
 	const { children } = $props();
+
+	$effect(() => {
+		// eslint-disable-next-line no-console
+		console.log(ALISUE);
+	});
 </script>
 
 <Header />
