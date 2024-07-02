8aab155fcde50bedf2ac08a9f0d1ac992c8e78b4
Tomoya Otake <tomoya.ton@gmail.com>
2024-07-03 01:10:10 +0900

    Use img tag instead of enhanced:img
    enhanced:img has unnecessary scrollbars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang='ts'>
+	import logo from '$/assets/vimjp-radio-cover-art/3000x3000-alpha-fs8.png';
 </script>
 
 <h1
 	uno-grid
 	uno-place='content-center items-center'
 >
-	<enhanced:img
-		alt='Vim-jp Radio Logo'
-		src='$/assets/vimjp-radio-cover-art/800x800-fs8.png'
+	<img
+		class='h-50svh object-contain'
+		alt='エンジニアの楽園 vim-jpラジオ'
+		src={logo}
 	/>
 </h1>
