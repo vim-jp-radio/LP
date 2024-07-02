6ddd0bef65ec206e82685bf40c8f15735903dd9c
Tomoya Otake <tomoya.ton@gmail.com>
2024-07-03 01:31:28 +0900

    Ignore enhanced:img closing tag warning

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,6 +4,7 @@
 
 {#if $page.error != null}
 	<div class='grid place-content-center place-items-center'>
+		<!-- svelte-ignore element_invalid_self_closing_tag -->
 		<enhanced:img alt='some alt text' src='$/assets/404.png' />
 		<h1>{$page.error.message}</h1>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	uno-m='0'
 	uno-place='content-center items-center'
 >
+	<!-- svelte-ignore element_invalid_self_closing_tag -->
 	<enhanced:img
 		class='h-auto max-w-100% w-50svh'
 		alt='エンジニアの楽園 vim-jpラジオ'
