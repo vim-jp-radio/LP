e2aab4b4d5f35d08173649414b7609cb2320916d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-02 16:22:41 +0100

    feat(unocss): presetAttributeにprefixを追加しておく
    参照
    https://unocss.dev/presets/attributify#properties-conflicts

--- uno.config.ts
+++ uno.config.ts
@@ -9,7 +9,7 @@ import {
 export default defineConfig({
 	presets: [
 		presetUno(), // defaultの設定。
-		presetAttributify(), // class属性ではなく、属性地に直接書く設定。https://unocss.dev/presets/attributify
+		presetAttributify({ prefix: 'uno-', prefixedOnly: true }), // class属性ではなく、属性地に直接書く設定。https://unocss.dev/presets/attributify
 		presetMini(), // Tailwind互換の最小限の設定を含む。 Aspect ration とか。 https://unocss.dev/presets/mini
 		presetIcons({ autoInstall: true }), // Iconを使うための設定。autoInstallも設定している。https://unocss.dev/presets/icons
 	],
