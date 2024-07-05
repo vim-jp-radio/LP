6af6c991f391cd15f11a0c36b78814f1172f9d10
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-05 16:15:30 +0100

    feat(Description): 段落わけ

--- src/routes/Description.svelte
+++ src/routes/Description.svelte
@@ -1,15 +1,23 @@
 <script lang='ts'>
 	const description = [
-		`エンジニアが集まるインターネット上のコミュニティvim-jpから生まれた初の音声プログラム。`,
-		`vim-jpはプログラミングから子育てに至るまで無数のチャンネルを抱えたコミュニティです。`,
-		`そんなコミュニティの面白さを生かして、各分野の様々なゲストを交えながら楽しく雑談していきます。`,
-		`配信日時は毎週月曜 昼12時です。`,
-		`なお、5週目はお休みとなります。`,
-	] as const satisfies string[];
+		[
+			`エンジニアが集まるインターネット上のコミュニティvim-jpから生まれた初の音声プログラム。`,
+			`vim-jpはプログラミングから子育てに至るまで無数のチャンネルを抱えたコミュニティです。`,
+			`そんなコミュニティの面白さを生かして、各分野の様々なゲストを交えながら楽しく雑談していきます。`,
+		],
+		[
+			`配信日時は毎週月曜 昼12時です。`,
+			`なお、5週目はお休みとなります。`,
+		],
+	] as const satisfies string[][];
 </script>
 
-<div uno-text='LP-foreground'>
-	{#each description as line (line)}
-		<p class='text'>{line}</p>
+<div uno-flex='~ col' uno-text='LP-foreground'>
+	{#each description as paragraph (paragraph)}
+		<div uno-gap='1.5'>
+			{#each paragraph as line (line)}
+				<p class='text'>{line}</p>
+			{/each}
+		</div>
 	{/each}
 </div>
