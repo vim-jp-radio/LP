f8f3b15a8b1924eb6d0afc5d090cafa74509d9fe
staticWagomU <to6wa17@gmail.com>
2024-07-05 23:49:07 +0900

    feat: 見出しコンポーネントの作成 text-shadowがunocssでは制御できなかったため、コンポーネントとして切り出す

---
+++ src/lib/Heading.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+	const { title }: { title: string } = $props();
+</script>
+
+<h2 uno-text='LP-blue 4xl'>
+	{title}
+</h2>
+
+<style>
+h2 {
+	text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #1ECFFF, 0 0 0.5em #1ECFFF, 0 0 0.1em #1ECFFF, 0 10px 3px #000;
+}
+</style>
