2094d81e8e11e02c55a4f3887f036f14fdbb9b4b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-05 11:12:39 +0100

    feat(unocss): transformerを設定(@apply使うため）

--- uno.config.ts
+++ uno.config.ts
@@ -4,6 +4,7 @@ import {
 	presetIcons,
 	presetMini,
 	presetUno,
+	transformerDirectives,
 } from 'unocss';
 
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
 		presetAttributify({ prefix: 'uno-', prefixedOnly: true }), // class属性ではなく、属性地に直接書く設定。https://unocss.dev/presets/attributify
 		presetMini(), // Tailwind互換の最小限の設定を含む。 Aspect ration とか。 https://unocss.dev/presets/mini
 		presetIcons({ autoInstall: true }), // Iconを使うための設定。autoInstallも設定している。https://unocss.dev/presets/icons
+	],
+	transformers: [
+		transformerDirectives(), // v-bindやv-modelなどのVueのディレクティブを使うための設定。https://unocss.dev/presets/directives
 	],
 	theme: {
 		colors: {
