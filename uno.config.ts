6c733e0f3c8311c4c648ff142df483ca0b0ccb00
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-05 11:13:03 +0100

    feat(unocss): webfontを使うのでpresetWebFontsを設定

--- uno.config.ts
+++ uno.config.ts
@@ -4,6 +4,7 @@ import {
 	presetIcons,
 	presetMini,
 	presetUno,
+	presetWebFonts,
 	transformerDirectives,
 } from 'unocss';
 
@@ -13,6 +14,9 @@ export default defineConfig({
 		presetAttributify({ prefix: 'uno-', prefixedOnly: true }), // class属性ではなく、属性地に直接書く設定。https://unocss.dev/presets/attributify
 		presetMini(), // Tailwind互換の最小限の設定を含む。 Aspect ration とか。 https://unocss.dev/presets/mini
 		presetIcons({ autoInstall: true }), // Iconを使うための設定。autoInstallも設定している。https://unocss.dev/presets/icons
+		presetWebFonts({
+			provider: 'google',
+		}), // Webフォントを使うための設定。preconnectも設定している。https://unocss.dev/presets/webfonts
 	],
 	transformers: [
 		transformerDirectives(), // v-bindやv-modelなどのVueのディレクティブを使うための設定。https://unocss.dev/presets/directives
