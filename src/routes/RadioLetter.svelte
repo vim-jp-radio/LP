a1c2c2b410ae98639e501fc0e29023a0cfaf822d
Tomoya Otake <tomoya.ton@gmail.com>
2024-07-06 13:49:31 +0900

    Wrap RadioLetter componet to use div for layout

--- src/routes/RadioLetter.svelte
+++ src/routes/RadioLetter.svelte
@@ -2,10 +2,12 @@
 	import Heading from '$/lib/Heading.svelte';
 </script>
 
-<Heading title='番組へのおたより' />
-<p>
-	この番組では皆さんからのおたよりを募集しています。 vim-jp Slackの #vim-jp-radio-message チャンネルに投稿してください。
-</p>
-<p>
-	Slackへのご参加は<a href='https://vim-jp.org/docs/chat.html'>こちら</a>から!
-</p>
+<div>
+	<Heading title='番組へのおたより' />
+	<p>
+		この番組では皆さんからのおたよりを募集しています。 vim-jp Slackの #vim-jp-radio-message チャンネルに投稿してください。
+	</p>
+	<p>
+		Slackへのご参加は<a href='https://vim-jp.org/docs/chat.html'>こちら</a>から!
+	</p>
+</div>
