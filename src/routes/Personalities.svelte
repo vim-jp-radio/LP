20d8cf9b92991a885eaf9a294570c82043e9914a
Tomoya Otake <tomoya.ton@gmail.com>
2024-07-06 13:35:17 +0900

    Create Personalities component

---
+++ src/routes/Personalities.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+	import Heading from '$/lib/Heading.svelte';
+</script>
+
+<div>
+	<Heading title='パーソナリティ' />
+	<div>
+		<div>
+			<h3>tomoya</h3>
+			<div>
+				<p uno-text>
+					1983年生まれ。起業家、及びフロントエンドからバックエンドまで幅広くカバーするWebエンジニア。 2010年に起業。2015年にイグジット。2017年にフォーコード株式会社を設立して、現在は日本で一番多くのVimmerと一緒にするEmacserとして、大企業システムを作ったりしています。著書に『Atom実践入門』、『［改訂新版］Emacs実践入門』、『CircleCI実践入門』（共に技術評論社）がある。
+				</p>
+			</div>
+		</div>
+		<div>
+			<h3>ありすえ</h3>
+			<div>
+				<p uno-text>
+					Vim/Neovimのプラグイン開発用エコシステムDenopsの作者。 小学校の時に「ゲームを作ってみたい」と思ったことがきっかけでプログラミングにハマる。 私生活のほとんどの時間をプログラミングに費やした。『Software Design』への寄稿や、『VimConf』、『Deno Fest』など多数のイベント登壇あり。トヨクモ株式会社の『Thanks OSS Award 2022』を受賞。好物は 🍣 と 🍺 と 🍜 。
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
