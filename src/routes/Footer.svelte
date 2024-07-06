4cda83899e9a8f22587993875f1f935c13678c7f
Tomoya Otake <tomoya.ton@gmail.com>
2024-07-06 13:46:40 +0900

    Create Footer component

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,21 @@
+<div>
+	<!-- svelte-ignore element_invalid_self_closing_tag -->
+	<enhanced:img
+		class='h-auto max-w-100% w-50svh'
+		alt='エンジニアの楽園 vim-jpラジオ'
+		src='$/assets/vimjp-radio-cover-art/3000x3000-alpha-fs8.png'
+	/>
+	<div>
+		TODO: ここにロゴが並ぶ
+	</div>
+	<div>
+		<p uno-color='#909296' uno-text-sm>
+			Personality: lambdalisue & tomoya<br />
+			Theme music: Jun Tanaka (GENTOUKI)<br />
+			Cover art design: Kazuaki Komai (こまゐ図考室)<br />
+			Production: Sha-la-la Company<br />
+			vim-jp radio team: atusy, conao3, emtee, gamoutatsumi, hakkadaikon, kawarimidoll, kyoh86, mikan, na0x2c6, peacock, ryoppippi, ryuichiroh, shishi, tani, and wagomu
+		</p>
+		<p uno-color='#909296' uno-text-sm>(c) Forcode Co., Ltd.</p>
+	</div>
+</div>
