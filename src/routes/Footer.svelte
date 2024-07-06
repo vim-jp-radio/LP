efc9b9326c04b0131746a57e4ff2e2928f9504be
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-06 18:39:14 +0100

    feat(Footer): marginを微調整

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,12 +2,12 @@
 	import * as Logo from '$lib/Logo';
 </script>
 
-<div>
-	<div uno-mb-32>
+<footer uno-pb-20 uno-pt-64>
+	<div uno-mb-24>
 		<Logo.VimJpRadio />
 	</div>
 
-	<div uno-flex uno-gap-4 uno-h-10 uno-justify-center uno-mb-16>
+	<div uno-flex uno-gap-4 uno-h-8 uno-justify-center uno-mb-8>
 		<Logo.Audee />
 		<Logo.ApplePodcast />
 		<Logo.Spotify />
@@ -16,14 +16,14 @@
 		<Logo.X />
 	</div>
 
-	<div>
-		<p uno-color='#909296' uno-text-sm>
+	<div uno-flex-col uno-space-y-4>
+		<p uno-color-LP-gray uno-text-sm>
 			Personality: lambdalisue & tomoya<br />
 			Theme music: Jun Tanaka (GENTOUKI)<br />
 			Cover art design: Kazuaki Komai (こまゐ図考室)<br />
 			Production: Sha-la-la Company<br />
 			vim-jp radio team: atusy, conao3, emtee, gamoutatsumi, hakkadaikon, kawarimidoll, kyoh86, mikan, na0x2c6, peacock, ryoppippi, ryuichiroh, shishi, tani, and wagomu
 		</p>
-		<p uno-color='#909296' uno-text-sm>(c) Forcode Co., Ltd.</p>
+		<p uno-color-LP-gray uno-text-sm>(c) Forcode Co., Ltd.</p>
 	</div>
-</div>
+</footer>
