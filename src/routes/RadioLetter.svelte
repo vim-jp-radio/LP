22cae5e5c5336d9189fee187741d2ff6c642955f
Tomoya Otake <tomoya.ton@gmail.com>
2024-07-06 13:53:14 +0900

    Set uno-text to RadioLetter p

--- src/routes/RadioLetter.svelte
+++ src/routes/RadioLetter.svelte
@@ -4,10 +4,10 @@
 
 <div>
 	<Heading title='番組へのおたより' />
-	<p>
+	<p uno-text>
 		この番組では皆さんからのおたよりを募集しています。 vim-jp Slackの #vim-jp-radio-message チャンネルに投稿してください。
 	</p>
-	<p>
+	<p uno-text>
 		Slackへのご参加は<a href='https://vim-jp.org/docs/chat.html'>こちら</a>から!
 	</p>
 </div>
