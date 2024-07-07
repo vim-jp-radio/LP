a748649ec283ea537fad82bc32ac4b1eea5c9ef8
Tomoya Otake <tomoya.ton@gmail.com>
2024-07-07 23:23:25 +0900

    Add images and link

--- src/routes/Platforms.svelte
+++ src/routes/Platforms.svelte
@@ -1,20 +1,32 @@
 <script lang='ts'>
 	import Heading from '$/lib/Heading.svelte';
+	import * as Logo from '$lib/Logo';
 </script>
 
 <div>
 	<Heading title='配信プラットフォーム' />
 	<div>
-		<p uno-text>
-			「エンジニアの楽園 vim-jpラジオ」は AuDee（TOKYO FM）の公式番組です。
-		</p>
+		<div>
+			<p uno-text>
+				「エンジニアの楽園 vim-jpラジオ」は AuDee（TOKYO FM）の公式番組です。
+			</p>
+			<a href='https://audee.jp/program/show/300008578' target='_blank'>AuDeeで聞く</a>
+		</div>
+		<Logo.AuDee />
 	</div>
 	<div>
 		<p uno-text>
 			ほかにもつぎのプラットフォームで配信しています。
 		</p>
-		<div>
-			TODO: ここにロゴが並ぶ
+		<div
+			uno-flex
+			uno-h-24
+			uno-justify-between
+		>
+			<Logo.ApplePodcast />
+			<Logo.Spotify />
+			<Logo.AmazonMusic />
+			<Logo.YouTube />
 		</div>
 	</div>
 </div>
