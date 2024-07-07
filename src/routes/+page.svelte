9f2987e702c49934fe0a86e9908ffd24359bb52e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-07 16:27:19 +0100

    画像をheader componentに移動

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,13 @@
 <script>
+	import Header from './Header.svelte';
 	import RadioLetter from './RadioLetter.svelte';
 	import Description from './Description.svelte';
 	import Platforms from './Platforms.svelte';
 	import Personalities from './Personalities.svelte';
 	import Footer from './Footer.svelte';
-	import { VimJpRadio } from '$lib/Logo';
 </script>
 
-<h1
-	uno-grid
-	uno-h-100svh
-	uno-m-0
-	uno-place='content-center items-center'
->
-	<VimJpRadio imageClass='h-auto max-w-100% w-50svh' />
-
-</h1>
+<Header />
 
 <main uno-flex-col uno-space-y-8>
 	<Description />
@@ -23,4 +15,5 @@
 	<Personalities />
 	<RadioLetter />
 </main>
+
 <Footer />
---
+++ src/routes/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { VimJpRadio } from '$lib/Logo';
+</script>
+
+<h1
+	uno-grid
+	uno-h-100svh
+	uno-m-0
+	uno-place='content-center items-center'
+>
+	<VimJpRadio imageClass='h-auto max-w-100% w-50svh' />
+
+</h1>
