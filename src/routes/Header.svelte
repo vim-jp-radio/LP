87823bca9f27ec01cd9eb0705775dedf884ce007
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-07 16:33:55 +0100

    feat(Header): `header` tagでかこむ

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,12 +2,13 @@
 	import { VimJpRadio } from '$lib/Logo';
 </script>
 
-<h1
-	uno-grid
-	uno-h-100svh
-	uno-m-0
-	uno-place='content-center items-center'
->
-	<VimJpRadio imageClass='h-auto max-w-100% w-50svh' />
-
-</h1>
+<header>
+	<h1
+		uno-grid
+		uno-h-100svh
+		uno-m-0
+		uno-place='content-center items-center'
+	>
+		<VimJpRadio imageClass='h-auto max-w-100% w-50svh' />
+	</h1>
+</header>
