59b39398f12e2d1b7a7f23d5bb63538467b0eb04
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-07 22:54:38 +0100

    feat(Header): 一番上までscrollされたらロゴを点滅させる

--- src/lib/utils/animation.svelte.ts
+++ src/lib/utils/animation.svelte.ts
@@ -17,3 +17,32 @@ export function restartAnimation<T extends HTMLElement>(element: T): void {
 		),
 	);
 }
+
+/**
+ * scroll を監視するClass
+ */
+export class ScrollXY {
+	#x = $state(0);
+	#y = $state(0);
+
+	constructor() {
+		if (!browser) {
+			throw new Error('This class can only be used in the browser environment.');
+		}
+
+		window.addEventListener('scroll', this.#update);
+	}
+
+	#update = () => {
+		this.#x = window.scrollX;
+		this.#y = window.scrollY;
+	};
+
+	get x() {
+		return this.#x;
+	}
+
+	get y() {
+		return this.#y;
+	}
+}
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,9 +1,30 @@
 <script lang='ts'>
+	import { browser, dev } from '$app/environment';
 	import { VimJpRadio } from '$lib/Logo';
-	import { restartAnimation } from '$lib/utils/animation.svelte.js';
+	import { ScrollXY, restartAnimation } from '$lib/utils/animation.svelte.js';
 
 	/** header 要素の参照 */
 	let header = $state<HTMLHeadElement>();
+
+	/* browserの時だけScrollXYを使う。windowに依存しているため */
+	const xyState = browser ? new ScrollXY() : undefined;
+
+	$effect(() => {
+		if (xyState == null) {
+			return;
+		}
+		const { y } = xyState;
+
+		/* devの時だけconsole.log */
+		if (dev) {
+			const { x, y } = xyState;
+			console.log({ x, y }); // eslint-disable-line no-console
+		}
+
+		if (header != null && y === 0) {
+			restartAnimation(header);
+		}
+	});
 
 /* eslint-disable svelte/no-unused-svelte-ignore */
 </script>
