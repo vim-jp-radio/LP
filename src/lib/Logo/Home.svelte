75b51d3046f175187c650b26de4231686671677c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-07 15:15:19 +0100

    feat(Logo): houseとgithubのロゴを追加

---
+++ src/lib/Logo/Home.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+	import Base from './_Base.svelte';
+	// @ts-ignore
+	import icon from '$/assets/logos/icon-home.png?enhanced';
+
+	const { link, alt, ...rest }: { link: string; alt: string } = $props();
+
+</script>
+
+<Base {alt} {icon} {link} {...rest} />
