b73aef9cf466be9b3a5cef03643a77de3a49a762
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-08 00:35:11 +0100

    fix(Platform): divにuno-buttonを移動、`uno.config.ts`に `uno-w-fit(width: fit-content)`を追加

--- src/routes/Platforms.svelte
+++ src/routes/Platforms.svelte
@@ -11,8 +11,8 @@
 				<p uno-text>
 					「エンジニアの楽園 vim-jpラジオ」は AuDee（TOKYO FM）の公式番組です。
 				</p>
-				<div>
-					<a href='https://audee.jp/program/show/300008578' target='_blank' uno-button><span>AuDee</span>で聞く</a>
+				<div uno-button>
+					<a href='https://audee.jp/program/show/300008578' target='_blank'><span>AuDee<span>で聞く</a>
 				</div>
 			</div>
 			<div uno-max-w-60 uno-min-w-36 uno-w-full>
--- uno.config.ts
+++ uno.config.ts
@@ -31,6 +31,6 @@ export default defineConfig({
 	},
 	shortcuts: {
 		text: 'text-base text-LP-text-color',
-		button: 'color-LP-yellow border-solid border-2 border-[var(--LP-yellow)] rounded px-4 py-2 hover:bg-[var(--LP-yellow)] hover:color-[var(--LP-backgroud)]',
+		button: 'color-LP-yellow border-solid border-2 border-[var(--LP-yellow)] rounded px-4 py-2 hover:bg-[var(--LP-yellow)] hover:color-[var(--LP-backgroud)] w-fit',
 	},
 });
