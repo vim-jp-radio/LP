b983b7a82e98d8c0c27272e2869edb791ea1accf
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-08 01:19:49 +0100

    feat(unocss): breakpoint tinyを追加(320px)

--- uno.config.ts
+++ uno.config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
 			'LP-backgroud': 'var(--LP-backgroud)',
 			'LP-text-color': 'var(--LP-text-color)',
 		},
+		breakpoints: {
+			tiny: '320px',
+		},
 	},
 	shortcuts: {
 		text: 'text-base text-LP-text-color',
