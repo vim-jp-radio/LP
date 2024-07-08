e5dc47970e156bbbd1838f40e61cbe8243602ff0
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-08 12:09:44 +0100

    feat(Logo): svgoによるsvg最適化を入れてみる（とはいえAuDeeロゴが1kb減っただけ)

---
+++ plugins/svgo.js
@@ -0,0 +1,71 @@
+/** @url https://github.com/r3dDoX/vite-plugin-svgo/blob/main/src/index.ts */
+
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import findCacheDirectory from 'find-cache-dir';
+import { optimize } from 'svgo';
+
+const VITE_PLUGIN_NAME = 'vite-plugin-svgo';
+const fileRegex = /\.svg$/;
+const cacheDir = findCacheDirectory({ name: VITE_PLUGIN_NAME });
+
+/**
+ * @typedef {import('svgo').Config} Config
+ * @typedef {import('vite').Plugin} Plugin
+ * @typedef {Omit<Config, 'path'>} PluginOptions
+ */
+
+/*
+  * This plugin will optimize SVG files using SVGO.
+  * @param {PluginOptions} svgoOptimizeOptions - SVGO options
+  * @returns {Plugin} - Vite plugin
+  */
+export default function viteSvgPlugin(svgoOptimizeOptions = {}) {
+	/** @type {Plugin} */
+	return {
+
+		/** @type {Plugin['name']} */
+		name: VITE_PLUGIN_NAME,
+
+		/** @type {Plugin['enforce']} */
+		enforce: 'pre',
+
+		/** @param {string} id */
+		async resolveId(id) {
+			if (typeof cacheDir !== 'string') {
+				throw new TypeError('Cache directory not found');
+			}
+
+			if (fileRegex.test(id) && !id.includes(cacheDir)) {
+				let svgCode;
+				try {
+					svgCode = await fs.readFile(id, 'utf8');
+				}
+				catch (exception) {
+					console.warn(`${id} couldn't be loaded by vite-plugin-svgo: `, exception);
+					return;
+				}
+				try {
+					const optimizedSvg = optimize(svgCode, {
+						path: id,
+						...svgoOptimizeOptions,
+					});
+
+					/* save optimized svg to cache */
+					await fs.mkdir(cacheDir, { recursive: true });
+
+					const basename = path.basename(id);
+					const cachePath = path.join(cacheDir, basename);
+
+					await fs.writeFile(cachePath, optimizedSvg.data, 'utf8');
+
+					console.log(`Optimized ${id} to ${cachePath}`); // eslint-disable-line no-console
+					return cachePath;
+				}
+				catch (exception) {
+					console.error(`${id} errored during svg optimization: `, exception);
+				}
+			}
+		},
+	};
+};
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite';
 import UnoCss from 'unocss/vite';
 import extractorSvelte from '@unocss/extractor-svelte';
 import { faviconPlugin } from './plugins/favicons';
+import Svgo from './plugins/svgo';
 
 export default defineConfig({
 	plugins: [
 		faviconPlugin,
+		Svgo(),
 		UnoCss({ extractors: [extractorSvelte()] }), // UnoCssの設定。extractorsにextractorSvelteを設定している。https://unocss.dev/extractors/svelte#svelte-extractor
 		enhancedImages(),
 		sveltekit(),
