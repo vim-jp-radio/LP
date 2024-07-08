f3225c8f55d28877e19815d522c705187c455b2b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-08 13:11:00 +0100

    Revert "headerロゴのanimation interactionを実装"

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,40 +1,9 @@
-<script lang='ts'>
-	import { browser, dev } from '$app/environment';
+<script>
 	import { VimJpRadio } from '$lib/Logo';
-	import { ScrollXY, restartAnimation } from '$lib/utils/animation.svelte.js';
 
-	/** header 要素の参照 */
-	let header = $state<HTMLHeadElement>();
-
-	/* browserの時だけScrollXYを使う。windowに依存しているため */
-	const xyState = browser ? new ScrollXY() : undefined;
-
-	$effect(() => {
-		if (xyState == null) {
-			return;
-		}
-		const { y } = xyState;
-
-		/* devの時だけconsole.log */
-		if (dev) {
-			const { x, y } = xyState;
-			console.log({ x, y }); // eslint-disable-line no-console
-		}
-
-		if (header != null && y === 0) {
-			restartAnimation(header);
-		}
-	});
-
-/* eslint-disable svelte/no-unused-svelte-ignore */
 </script>
 
-<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
-<header
-	bind:this={header}
-	onclick={() => (header != null) && restartAnimation(header)}
-	uno-animated='duration-500ms flash repeat-1.5 delay-500ms'
->
+<header uno-animated='duration-500ms flash repeat-1.5 delay-500ms'>
 	<h1
 		uno-grid
 		uno-h-100svh
