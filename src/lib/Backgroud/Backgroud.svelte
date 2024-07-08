721be57b723fd408353ae3317f1a437d8bb0ee19
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-08 13:52:22 +0100

    feat(Background): prefers-reduced-motion==true ならば、Circleの動きを止める

--- src/lib/Backgroud/Backgroud.svelte
+++ src/lib/Backgroud/Backgroud.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 	import { animate, createCircles } from './circle.js';
+	import { PrefersReducedMotion } from '$lib/utils/runes.svelte.js';
 
 	/**
 	 * @param {number} circleNum - 円の数
@@ -15,6 +16,8 @@
 		minSpeed?: number;
 		maxSpeed?: number;
 	} = $props();
+	/** media queryのprefers-reduced-motionを取得 */
+	const prefersReducedMotionRune = new PrefersReducedMotion();
 
 	let canvas = $state<HTMLCanvasElement | undefined>(undefined);
 	const ctx = $derived.by(() => canvas?.getContext('2d'));
@@ -25,7 +28,7 @@
 	let jsLoaded = $state(false);
 
 	const circles = $derived.by(() =>
-		canvas != null && ctx != null ? createCircles(circleNum, canvas, ctx, minSpeed, maxSpeed) : [],
+		canvas != null && ctx != null ? createCircles(circleNum, canvas, ctx, minSpeed, maxSpeed, prefersReducedMotionRune.isReduced) : [],
 	);
 
 	/** js が読み込まれたらオンになる */
