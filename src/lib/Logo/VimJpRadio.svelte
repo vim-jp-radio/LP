fa3cf7500cf14bb39cabdddce5b1b367c9543931
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-08 11:10:27 +0100

    feat(Logo): 画像サイズを複数生成してみる

--- src/lib/Logo/VimJpRadio.svelte
+++ src/lib/Logo/VimJpRadio.svelte
@@ -4,7 +4,7 @@
 	import Base from './_Base.svelte';
 
 	// @ts-ignore
-	import icon from '$/assets/vimjp-radio-cover-art/3000x3000-alpha-fs8.png?enhanced';
+	import icon from '$/assets/vimjp-radio-cover-art/3000x3000-alpha-fs8.png?enhanced&w=400;800;1200;1600;2400;3000';
 
 	const alt = `エンジニアの楽園 vim-jpラジオ`;
 </script>
