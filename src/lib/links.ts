0479f4a24325d6c046c40bbc059bcb40d0d74f46
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-09 18:18:13 +0100

    feat(LINKS): labelを追加

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -3,20 +3,20 @@ import { building } from '$app/environment';
 
 /** リンク集 */
 export const LINKS = {
-	VimJpRadio: `https://vim-jp-radio.com/`,
-	VimJpSlack: `https://vim-jp.org/docs/chat.html`,
-	AmazonMusic: `https://music.amazon.co.jp/podcasts/e059ff85-b9a1-499e-9cc5-b148e5b56c18/エンジニアの楽園-vim-jp-ラジオ`,
-	ApplePodcast: `https://podcasts.apple.com/jp/podcast/エンジニアの楽園-vim-jp-ラジオ/id1755104750`,
-	Spotify: `https://open.spotify.com/show/3fkxQ6g3577mog02HuXZMj?nd=1&dlsi=c2f29ebcdc1940ab`,
-	X: `https://x.com/vimjpradio`,
-	AuDee: `https://audee.jp/program/show/300008578`,
-	Youtube: `https://www.youtube.com/playlist?list=PLcptmT4PuRVNm5qjf5DhzPYZenncjLWQ8`,
-} as const;
+	VimJpRadio: { url: `https://vim-jp-radio.com/`, label: `エンジニアの楽園 - vim-jp ラジオ` },
+	VimJpSlack: { url: `https://vim-jp.org/docs/chat.html`, label: `vim-jp Slack` },
+	AmazonMusic: { url: `https://music.amazon.co.jp/podcasts/e059ff85-b9a1-499e-9cc5-b148e5b56c18/エンジニアの楽園-vim-jp-ラジオ`, label: `Amazon Music` },
+	ApplePodcast: { url: `https://podcasts.apple.com/jp/podcast/エンジニアの楽園-vim-jp-ラジオ/id1755104750`, label: `Apple Podcast` },
+	Spotify: { url: `https://open.spotify.com/show/3fkxQ6g3577mog02HuXZMj?nd=1&dlsi=c2f29ebcdc1940ab`, label: `Spotify` },
+	X: { url: `https://x.com/vimjpradio`, label: `X` },
+	AuDee: { url: `https://audee.jp/program/show/300008578`, label: `AuDee` },
+	Youtube: { url: `https://www.youtube.com/playlist?list=PLcptmT4PuRVNm5qjf5DhzPYZenncjLWQ8`, label: `Youtube` },
+} as const satisfies Record<string, { url: string; label: string }>;
 
 /* ビルド時にURLをチェックする */
 if (building) {
 	for (const key in LINKS) {
-		const link = LINKS[key as keyof typeof LINKS];
-		ensureURL(link);
+		const { url } = LINKS[key as keyof typeof LINKS];
+		ensureURL(url);
 	}
 }
