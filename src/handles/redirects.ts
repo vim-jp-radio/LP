2ab3fd637ee3e2176fb7f1f4f4e1795ae35f5db6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-10 09:48:05 +0100

    fix(redirects): アクセスがあるたびに`_redirects` fileを毎回読みに行くように修正。初回起動時に`_redirects` fileがないとdev serverの起動がエラーになる問題を修正

--- src/handles/redirects.ts
+++ src/handles/redirects.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import { existsSync } from 'node:fs';
 import fs from 'node:fs/promises';
 import { type Handle, redirect } from '@sveltejs/kit';
 import { createRedirect } from 'cloudflare-redirect-parser';
@@ -8,19 +9,21 @@ import { createRedirect } from 'cloudflare-redirect-parser';
  * base は 下のライブラリ
  * @see https://github.com/bluwy/cloudflare-redirect-parser
  */
-export async function redirects(): Promise<Handle> {
+export const redirects = (async ({ event, resolve }) => {
 	const redirectFile = path.join(import.meta.dirname, '../static/_redirects');
+
+	if (!existsSync(redirectFile)) {
+		return resolve(event);
+	}
 	const redirectContent = await fs.readFile(redirectFile, 'utf-8');
 	const redirectF = createRedirect(redirectContent);
 
-	return async ({ event, resolve }) => {
-		const result = redirectF(event.url.pathname);
+	const result = redirectF(event.url.pathname);
 
-		if (result != null) {
-			const { status, to } = result;
-			return redirect(status, to);
-		}
+	if (result != null) {
+		const { status, to } = result;
+		return redirect(status, to);
+	}
 
-		return resolve(event);
-	};
-}
+	return resolve(event);
+}) satisfies Handle;
