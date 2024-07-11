5409e2011ff3e4424264cca15337eff70b209721
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-11 09:53:29 +0100

    feat(unocss): theme関数を使ってカラーを取り出す

--- src/lib/Heading/Heading.svelte
+++ src/lib/Heading/Heading.svelte
@@ -8,6 +8,8 @@
 
 <style>
 h2 {
-	text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em var(--LP-blue), 0 0 0.5em var(--LP-blue), 0 0 0.1em var(--LP-blue), 0 10px 3px var(--LP-backgroud);
+  --blue: theme('colors.LP-blue');
+  --background: theme('colors.LP-backgroud');
+	text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em var(--blue), 0 0 0.5em var(--blue), 0 0 0.1em var(--blue), 0 10px 3px var(--background);
 }
 </style>
