a7eeee7e0d65008aba78e2f2ab88c48462b6f84d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-11 07:15:01 +0100

    feat(Logo): Suzuri のリンクを追加

--- src/lib/Logo/Suzuri.svelte
+++ src/lib/Logo/Suzuri.svelte
@@ -1,15 +1,12 @@
 <script lang='ts'>
-	import type { ComponentProps } from 'svelte';
 	import { LINKS } from '../links';
 	import Base from './_Base.svelte';
 	// @ts-ignore
 	import icon from '$/assets/logos/suzuri-logo-white.png?enhanced&w=300;600;800';
 
-	const { ...rest }: Omit<ComponentProps<Base>, 'icon'> = $props();
+	const { ...rest } = $props();
 
-	const { label: alt, url } = LINKS.X;
-
-	const link = rest.link ?? url;
+	const { label: alt, url: link } = LINKS.Suzuri;
 </script>
 
 <Base {alt} {icon} {link} {...rest} />
