62b089f476c7324b0f2a5d99f592d20feb68d916
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-11 08:58:55 +0100

    fix(Description.svelte): 普通に説明を書き下して書く

--- src/routes/Description.svelte
+++ src/routes/Description.svelte
@@ -1,26 +1,11 @@
-<script lang='ts'>
-	const description = [
-		[
-			`エンジニアが集まるインターネット上のコミュニティvim-jpから生まれた初の音声プログラム。`,
-			`vim-jpはプログラミングから子育てに至るまで無数のチャンネルを抱えたコミュニティです。`,
-			`そんなコミュニティの面白さを生かして、各分野の様々なゲストを交えながら楽しく雑談していきます。`,
-		],
-		[
-			`配信日時は毎週月曜 昼12時です。`,
-			`なお、5週目はお休みとなります。`,
-		],
-	] as const satisfies string[][];
-</script>
-
 <section uno-flex='~ col' uno-gap-1.5>
-	{#each description as paragraph (paragraph)}
-		<p class='text'>
-			{#each paragraph as line, i (line)}
-				{line}
-				{#if i < paragraph.length - 1}
-					<br />
-				{/if}
-			{/each}
-		</p>
-	{/each}
+	<p uno-text>
+		エンジニアが集まるインターネット上のコミュニティvim-jpから生まれた初の音声プログラム。<br />
+		vim-jpはプログラミングから子育てに至るまで無数のチャンネルを抱えたコミュニティです。<br />
+		そんなコミュニティの面白さを生かして、各分野の様々なゲストを交えながら楽しく雑談していきます。
+	</p>
+	<p uno-text>
+		配信日時は毎週月曜 昼12時です。<br />
+		なお、5週目はお休みとなります。
+	</p>
 </section>
