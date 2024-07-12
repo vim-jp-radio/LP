4ca0adfe6f8731c564e0c1190a6127895bd29514
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-12 14:04:43 +0100

    feat(redirect): `_redirects` の生成を `@ryoppippi/vite-plugin-cloudflare-redirectvite-plugin-cloudflare-redirect`に移行する
    https://github.com/ryoppippi/vite-plugin-cloudflare-redirect
    https://jsr.io/@ryoppippi/vite-plugin-cloudflare-redirect

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,5 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import { meta } from './handles/meta';
-import { redirects } from './handles/redirects';
-import { dev } from '$app/environment';
 
 /** 最終的な hooks */
-export const handle = sequence(
-	...[
-		meta, // head に favicon を追加する
-		dev ? redirects : [], // `_redirects` を元にリダイレクトを行う (dev 時のみ)
-	].flat(),
-);
+export const handle = sequence(meta);
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
+import path from 'node:path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 import UnoCss from 'unocss/vite';
 import extractorSvelte from '@unocss/extractor-svelte';
-import { faviconPlugin, generateRedirects } from './plugins';
+import { cloudflareRedirect } from '@ryoppippi/vite-plugin-cloudflare-redirect';
+import { LINKS } from './src/lib/links/_links.js';
+import { faviconPlugin } from './plugins';
+
+function relativePath(...args: string[]): string {
+	return path.resolve(import.meta.dirname, ...args);
+}
 
 export default defineConfig({
 	plugins: [
 		faviconPlugin,
-		generateRedirects,
+
+		/* `_recirects`ファイルを生成するための設定 */
+		cloudflareRedirect({
+			mode: 'generate',
+			redirectsFilePath: relativePath('./static/_redirects'),
+			entries: [
+				{ from: '/audee', to: LINKS.AuDee.url },
+				{ from: '/apple', to: LINKS.ApplePodcast.url },
+				{ from: '/amazon', to: LINKS.AmazonMusic.url },
+				{ from: '/spotify', to: LINKS.Spotify.url },
+				{ from: '/youtube', to: LINKS.Youtube.url },
+				{ from: '/suzuri', to: LINKS.Suzuri.url },
+			],
+		}),
+
 		UnoCss({ extractors: [extractorSvelte()] }), // UnoCssの設定。extractorsにextractorSvelteを設定している。https://unocss.dev/extractors/svelte#svelte-extractor
 		enhancedImages(),
 		sveltekit(),
