c1e37532fb8de0522e18dad629012ab2f8ddbe06
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-12 21:32:49 +0100

    refactor(unocss): presetMiniを削除（presetUno に含まれているらしい)

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,6 @@ import {
 	defineConfig,
 	presetAttributify,
 	presetIcons,
-	presetMini,
 	presetUno,
 	transformerDirectives,
 	transformerVariantGroup,
@@ -13,7 +12,6 @@ export default defineConfig({
 	presets: [
 		presetUno(), // defaultの設定。
 		presetAttributify({ prefix: 'uno-', prefixedOnly: true }), // class属性ではなく、属性地に直接書く設定。https://unocss.dev/presets/attributify
-		presetMini(), // Tailwind互換の最小限の設定を含む。 Aspect ration とか。 https://unocss.dev/presets/mini
 		presetIcons({ autoInstall: isDevelopment }), // Iconを使うための設定。autoInstallも設定している。https://unocss.dev/presets/icons
 	],
 	transformers: [
