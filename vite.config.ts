702c2f838ab74e850225e07d088fb8691845f77b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-12 17:18:24 +0100

    refactor(favicon): pluginの設定をviteでできるように

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,47 @@ import { defineConfig } from 'vite';
 import UnoCss from 'unocss/vite';
 import extractorSvelte from '@unocss/extractor-svelte';
 import { cloudflareRedirect } from '@ryoppippi/vite-plugin-cloudflare-redirect';
-import { LINKS } from './src/lib/links/_links.js';
 import { faviconPlugin } from './plugins';
 
+import UnocssConfig from './uno.config.js';
+import { LINKS, VIM_JP_RADIO_INFO } from './src/lib/links/_links.js';
+
 function relativePath(...args: string[]): string {
+	// @ts-expect-error import.meta.dirname  is not defined in typescript
+	// eslint-disable-next-line ts/no-unsafe-argument
 	return path.resolve(import.meta.dirname, ...args);
+}
+
+/* unocss から background color を取得 */
+// @ts-expect-error background color may be undefined
+// eslint-disable-next-line ts/no-unsafe-member-access
+const background = UnocssConfig?.theme?.colors?.['LP-backgroud'] as unknown;
+
+if (typeof background !== 'string') {
+	throw new TypeError('background color is not defined');
 }
 
 export default defineConfig({
 	plugins: [
-		faviconPlugin,
+		/* favicon と metadata の設定 */
+		faviconPlugin({
+			/** ===== faviconsの設定 ===== */
+			imgSrc: relativePath('./src/assets/vimjp-radio-cover-art/3000x3000-fs8.png'),
+			dest: relativePath('./static/favicons'),
+			htmlDest: relativePath('./src/assets/favicons.html'),
+			/** ========================= */
+
+			/* ===== metadataの設定 ===== */
+			path: `/favicons`,
+			theme_color: background,
+			background,
+			appName: VIM_JP_RADIO_INFO.title,
+			appShortName: VIM_JP_RADIO_INFO.title,
+			appDescription: VIM_JP_RADIO_INFO.description,
+			lang: 'ja-JP',
+			orientation: 'portrait',
+			/* ========================= */
+		}),
 
 		/* `_recirects`ファイルを生成するための設定 */
 		cloudflareRedirect({
