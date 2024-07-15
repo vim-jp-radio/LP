b5dc9c9805387b61542df4c6acdb0311c47eeef3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-15 17:50:53 +0100

    feat(Tweet): Tweet componentをdynamic importにする

--- src/routes/ListenersTweets.svelte
+++ src/routes/ListenersTweets.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-	import { type ITweet, Tweet } from 'sveltekit-tweet';
+	import type { ITweet } from 'sveltekit-tweet';
 	import { Heading } from '$/lib/Heading';
 
 	const { tweets }: { tweets: ITweet[] } = $props();
@@ -7,9 +7,11 @@
 
 <section>
 	<Heading title='リスナーの声' />
-	<div data-theme='dark'>
-		{#each tweets as tweet (tweet.id_str)}
-			<Tweet {tweet} />
-		{/each}
-	</div>
+	{#await import('sveltekit-tweet') then { Tweet }}
+		<div data-theme='dark'>
+			{#each tweets as tweet (tweet.id_str)}
+				<Tweet {tweet} />
+			{/each}
+		</div>
+	{/await}
 </section>
