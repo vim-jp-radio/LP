bc23e62c2a35f3aba1e8dd85a289df18bee6960b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-26 23:12:14 +0100

    feat(+page.server.ts): tweet を並列に取得するよう修正

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,18 @@
 import { error } from '@sveltejs/kit';
 import { getTweet } from 'sveltekit-tweet/server';
-import type { ITweet } from 'sveltekit-tweet';
 import { LISTENERS_TWEET_IDS } from '$/lib/links';
 
 /* tweet を取得する。 */
 export async function load() {
-	const tweets: ITweet[] = [];
-	for (const id of LISTENERS_TWEET_IDS) {
-		const tweet = await getTweet(id);
-		if (tweet == null) {
-			error(404, `Tweet not found: ${id}`);
-		}
-		tweets.push(tweet);
-	}
+	const tweets = LISTENERS_TWEET_IDS.map(
+		async id => getTweet(id)
+			.then((tweet) => {
+				if (tweet == null) {
+					error(404, `Tweet not found: ${id}`);
+				}
+				return tweet;
+			}),
+	);
 
-	return { tweets };
+	return { tweets: await Promise.all(tweets) };
 }
