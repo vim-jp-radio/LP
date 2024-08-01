7ec08df1aa087715819ba4cd4029011fde884165
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-01 14:15:57 +0100

    feat: replace sveltekit-tweet with sveltweet
    The sveltekit-tweet package has been replaced with the sveltweet
    package. This change affects the import statements and the type
    definitions in the source code. The affected files are
    src/routes/+page.server.ts and src/routes/ListenersTweets.svelte.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
 import { error } from '@sveltejs/kit';
-import { getTweet } from 'sveltekit-tweet/server';
-import type { ITweet } from 'sveltekit-tweet';
+import { getTweet } from 'sveltweet/api';
+import type { Tweet } from 'sveltweet';
 import { LISTENERS_TWEET_IDS } from '$/lib/links';
 
 /* tweet を取得する。 */
 export async function load() {
-	const tweets: ITweet[] = [];
+	const tweets: Tweet[] = [];
 	for (const id of LISTENERS_TWEET_IDS) {
 		const tweet = await getTweet(id);
 		if (tweet == null) {
