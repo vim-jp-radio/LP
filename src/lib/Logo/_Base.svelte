0847cec4d103b006bbfca8bfae1c0ffc92dd06dc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-05 18:28:34 +0100

    feat(Logo/_Base): use Image.svelte

--- src/lib/Logo/_Base.svelte
+++ src/lib/Logo/_Base.svelte
@@ -1,36 +1,22 @@
 <script lang='ts'>
-	import type dummyEnhanced from '*?enhanced';
 	import type { HTMLImgAttributes } from 'svelte/elements';
+	import Image from '../Image.svelte';
 	import { ensureURL } from '../utils/url';
 
-	type Picture = typeof dummyEnhanced;
-
-	const { link, icon, alt, class: className, ...rest }: { link?: string; icon: string | Picture } & Omit<HTMLImgAttributes, 'src'> = $props();
+	const { link, icon, alt, class: className, ...rest }: { link?: string; icon: string } & Omit<HTMLImgAttributes, 'src'> = $props();
 
 	const _defaultClassName = `h-full object-scale-down w-auto`;
 	const _className = className ?? _defaultClassName;
 	const loading: typeof rest.loading = 'loading' in rest ? rest.loading : `lazy`;
-
 </script>
 {#snippet image()}
-{#if typeof icon === 'string' && icon.endsWith('.svg')}
-<!-- svg image -->
-<img
-	class={_className}
-	{alt}
-	{loading}
-	src={icon}
-	{...rest}
-/>
-{:else}
-	<enhanced:img
+	<Image
 		class={_className}
-		{alt}
+		alt={alt as string}
 		{loading}
 		src={icon}
 		{...rest}
 	/>
-  {/if}
 {/snippet}
 
 {#if link != null}
