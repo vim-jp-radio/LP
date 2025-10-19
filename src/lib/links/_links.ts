9b1a95eced98edaec068c1d6d352589bf641a023
conao3 <conao3@gmail.com>
2025-10-20 01:57:13 +0900

    use radiko as primary link

--- src/lib/links/_links.ts
+++ src/lib/links/_links.ts
@@ -7,6 +7,7 @@ export const LINKS = {
 	VimJpSlack: { url: `https://vim-jp.org/docs/chat.html`, label: `vim-jp Slack` },
 	AmazonMusic: { url: `https://music.amazon.co.jp/podcasts/e059ff85-b9a1-499e-9cc5-b148e5b56c18/エンジニアの楽園-vim-jp-ラジオ`, label: `Amazon Music` },
 	ApplePodcast: { url: `https://podcasts.apple.com/jp/podcast/エンジニアの楽園-vim-jp-ラジオ/id1755104750`, label: `Apple Podcast` },
+	Radiko: { url: `https://radiko.jp/podcast/channels/bf2c6e2d-9fe1-448f-b08a-523c93a5b316`, label: `radiko` },
 	Spotify: { url: `https://open.spotify.com/show/3fkxQ6g3577mog02HuXZMj?nd=1&dlsi=c2f29ebcdc1940ab`, label: `Spotify` },
 	X: { url: `https://x.com/vimjpradio`, label: `X` },
 	Youtube: { url: `https://www.youtube.com/playlist?list=PLcptmT4PuRVNm5qjf5DhzPYZenncjLWQ8`, label: `Youtube` },
